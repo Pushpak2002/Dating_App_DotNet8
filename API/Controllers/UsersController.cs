32c4404b4ae2d69d7bc0fc496a17098fe7ca0eda
Pushpak2002 <kamblepushpak27@gmail.com>
2024-07-20 22:33:17 +0530

    Day 4 - Authentication

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+
+
+public class UsersController(DataContext context):BaseApiController
+{
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+
+        return users;
+    }
+
+
+    [Authorize]
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUsers(int id)
+    {
+        var users = await context.Users.FindAsync(id);
+
+        if(users == null) return NotFound();
+
+        return users;
+    }
+}
---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DTOs;
+
+public class LoginDto
+{
+    public required string Username { get; set; }
+
+    public required string password { get; set; }
+}
